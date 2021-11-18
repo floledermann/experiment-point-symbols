2ecfe2a21fbc59268dc687a2d7e0b874d55faa4c
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-11-18 14:53:55 +0100

    adjust watched files

--- run-monitor.bat
+++ run-monitor.bat
@@ -1,4 +1,4 @@
 
-call npx nodemon --watch ./*.js --watch package.json --watch src --watch ../stimsrv/src --exec "npx" stimsrv experiment.js
+call npx nodemon --watch ./*.js --watch package.json --watch tasks --watch ../stimsrv/src --exec "npx" stimsrv experiment.js
 
 pause
