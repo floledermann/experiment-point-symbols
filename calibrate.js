fb0988182b1bf602c862b9a9f5f6c6a64e83233a
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2022-01-28 11:43:12 +0100

    update calibration task

--- calibrate.js
+++ calibrate.js
@@ -1,18 +1,11 @@
 
-const snellen = require("stimsrv/task/snellen");
-const tao = require("stimsrv/task/aucklandoptotypes");
+const snellen = require("stimsrv/task/tumblingE");
 const pause = require("stimsrv/task/pause");
 const loop = require("stimsrv/task/loop");
 
-const staircase = require("stimsrv/controller/staircase");
-const random = require("stimsrv/controller/random");
 const sequence = require("stimsrv/controller/sequence");
 
 const filestorage = require("stimsrv/storage/filestorage");
-
-const centerline = require("./src/task/centerline.js");   
-const dashedline = require("./src/task/dashedline.js");  
-const text = require("./src/task/text.js");  
 
 const resource = require("stimsrv/util/resource");
 
